646f2f276aa5db807dcfb098d15250a4dda72fea
Philipp <philipphonsel@gmail.com>
2021-11-06 18:04:38 +0100

    Add JavaDoc Comments

--- src/whs/fpr/AxBMatrix.java
+++ src/whs/fpr/AxBMatrix.java
@@ -1,5 +1,11 @@
 package whs.fpr;
 
+/**
+ * Diese Klasse stellt eine A x = B Matrix dar. Reihen (Gleichungen) werden in double Arrays gespeichert.
+ * Der letzte Wert einer Reihe stellt somit immer das Ergebnis der Gleichung dar.
+ * @author Philipp Honsel
+ * @version 1
+ */
 public class AxBMatrix {
 
     public final int count;
@@ -12,25 +18,38 @@ public class AxBMatrix {
         _matrix = new double[count][width];
     }
 
+    /**
+     * Erstellt automatisch eine AxBMatrix Instanz, welche bereits mit Gleichungen befüllt ist.
+     * Es stehen mehrere Matrizen zur Auswahl
+     * @param version Die Id der Matrix, welche erstellt werden soll
+     * @return Instanz der Debugmatrix
+     */
     public static AxBMatrix getDebugMatrix(int version){
         AxBMatrix matrix = new AxBMatrix(3);
 
         switch (version) {
-            case 1 -> {
+            default:
+            case 1:
                 matrix.setRow(0, new double[]{1, 2, 3}, 2);
                 matrix.setRow(1, new double[]{1, 1, 1}, 2);
                 matrix.setRow(2, new double[]{3, 3, 1}, 0);
-            }
-            case 2 -> {
+                break;
+            case 2:
                 matrix.setRow(0, new double[]{7, 3, -5}, -12);
                 matrix.setRow(1, new double[]{-1, -2, 4}, 5);
                 matrix.setRow(2, new double[]{-4, 1, -3}, 1);
-            }
+                break;
         }
 
         return matrix;
     }
 
+    /**
+     * Überschreibt eine Gleichung in der Matrix.
+     * @param index Spalte der Matrix, welche überschrieben werden soll
+     * @param values Die Werte der A Matrix (Gleichung)
+     * @param result Der Wert des B Vektor (Ergebnis)
+     */
     public void setRow(int index, double[] values, double result){
         if(values.length != count){
             throw new IllegalArgumentException("Row needs to be of length " + count);
@@ -47,12 +66,23 @@ public class AxBMatrix {
         _matrix[index] = rowValues;
     }
 
+    /**
+     * Subtrahiert die übergebenen Werte von einer gewählten Spalte. Dabei werden jeweils die Werte von
+     * x0 von x0, x1 von x1, ..., xn von xn subtrahiert.
+     * @param index Reihe, welche von welcher subtrahiert werden soll
+     * @param rowValues Werde, welche abgezogen werden sollen
+     */
     public void subtractRow(int index, double[] rowValues){
         for(int i = 0; i <= count; i++){
             getRow(index)[i] = getRow(index)[i] - rowValues[i];
         }
     }
 
+    /**
+     * Vertauscht die Reihen mit den gegebenen Indizes
+     * @param sourceIndex Von der Reihe
+     * @param targetIndex In die Reihe
+     */
     public void swapRow(int sourceIndex, int targetIndex){
         double[] targetCopy = _matrix[targetIndex];
         _matrix[targetIndex] = _matrix[sourceIndex];
@@ -63,6 +93,10 @@ public class AxBMatrix {
         return _matrix[index];
     }
 
+    /**
+     * Gibt die Matrix in der Konsole aus. Die Werte werden auf 2 Nachkommastellen gerundet.
+     *
+     */
     public void printMatrix(){
         System.out.println();
         System.out.printf("Gauss Matrix of Size: %d%n", count);
--- src/whs/fpr/GaussElimination.java
+++ src/whs/fpr/GaussElimination.java
@@ -2,10 +2,18 @@ package whs.fpr;
 
 
 /**
- *
+ * Diese Klasse stellt alle Methoden bereit, welche benötigt werden, um eine AxBMatrix zu lösen.
+ * @author Philipp Honsel
+ * @version 1
  */
 public class GaussElimination {
 
+    /**
+     * Berechnet aus einer beliebigen Matrix die Echelon Form. Diese Form hat in einer Ecke der Matrix nur Nullen.
+     * Die Ecke ist beliebig, wird in dieser Implementierung allerdings unten links sein.
+     * @param matrix Matrix, für welche die Echelon Form errechnet werden soll
+     * @return Matrix in Echelon Form
+     */
     public static AxBMatrix toEchelonForm(AxBMatrix matrix){
         for(int currentRow = 0; currentRow < matrix.count - 1; currentRow++){
             int bestDiagonalRow = findOptimalPivotRow(matrix, currentRow + 1, /*row == diagonalIndex*/ currentRow);
@@ -23,6 +31,11 @@ public class GaussElimination {
         return matrix;
     }
 
+    /**
+     * Errechnet aus einer Matrix in Echelon Form die Lösung der Matrix
+      * @param matrix Matrix in Echelon Form
+     * @return Array mit der Lösung der Matrix. Dabei ist Index 0 die Lösung für x0 etc.
+     */
     public static double[] solveFromEchelonForm(AxBMatrix matrix){
         if(!isEchelonForm(matrix)){
             throw new RuntimeException("Cannot calculate from echelon form, if the matrix is not in echelon form");
@@ -44,6 +57,11 @@ public class GaussElimination {
         return results;
     }
 
+    /**
+     * Löst eine Gleichung in Matrix Form. Dabei ist es egal ob diese bereits in Echelon Form vorliegt.
+     * @param matrix Matrix welche gelöst werden soll
+     * @return Array mit der Lösung der Matrix. Dabei ist Index 0 die Lösung für x0 etc.
+     */
     public static double[] solve(AxBMatrix matrix) {
         if(isEchelonForm(matrix)){
             return solveFromEchelonForm(matrix);
@@ -52,6 +70,11 @@ public class GaussElimination {
         return solveFromEchelonForm(toEchelonForm(matrix));
     }
 
+    /**
+     * Überprüft ob eine Matrx in Echelon Form vorliegt. Diese Methode erkennt nur die untere linke Ecke.
+     * @param matrix Matrix welche überprüft werden soll
+     * @return true wenn Matrix in Echelon Form vorliegt.
+     */
     public static boolean isEchelonForm(AxBMatrix matrix){
         for(int i = 0; i < matrix.count; i++){
             double[] row = matrix.getRow(i);
